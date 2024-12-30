58ee3c799e7526c7f5ecf4f814d7d2888a28773a
BenC-7897 <ben.crane.a@gmail.com>
2024-12-30 11:39:40 +0000

    Update Csv_Rows.py

--- Csv_Rows.py
+++ Csv_Rows.py
@@ -1,6 +1,6 @@
 import pandas as pd # Importing the pandas library 
 import glob # Importing the glob module 
-files = glob.glob('C:/Users/username/Desktop/file folder/data file folder//*.csv') # Use glob to find all .csv files in the specified directory 
+files = glob.glob('C:/Users/username/Desktop/file folder/data file folder/file, *.csv') # Use glob to find all .csv files in the specified directory 
 dfs = [] # Initialise an empty list to store Data Frames 
 for csv in files: # Loop through each .csv file found by glob 
    temp_df = pd.read_csv(csv) # Read the .csv file into a Data Frame 
