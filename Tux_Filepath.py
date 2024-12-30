3f6f28700300fe055d827c8806622bf7db2a1d57
BenC-7897 <ben.crane.a@gmail.com>
2024-12-30 16:27:06 +0000

    Update Tux_Filepath.py

--- Tux_Filepath.py
+++ Tux_Filepath.py
@@ -1,46 +1,30 @@
 import os  # Importing the 'os' module to interact with the operating system
 
-# Prompt the user to input a file or directory path
-filepath = input("Please enter the file path: ")
+filepath = input("Please enter the file path: ") # Prompt the user to input a file or directory path
 
-# Print the entered file path for confirmation
-print(filepath)
+print(filepath) # Print the entered file path for confirmation
 
-# Check if the specified path exists
-if os.path.exists(filepath):
+if os.path.exists(filepath): # Check if the specified path exists
     print('The path exists')  # Inform the user that the path exists
     
-    # Check if the path is a file
-    if os.path.isfile(filepath):
-        # Split the file path to get the file extension
-        _, ext = os.path.splitext(filepath)
+    if os.path.isfile(filepath): # Check if the path is a file
+        _, ext = os.path.splitext(filepath) # Split the file path to get the file extension
 
-        # Check if the file extension is '.tux'
-        if ext == '.tux':
-            # Open the .tux file in read mode with the specified encoding
-            with open(filepath, 'r', encoding='utf-8-sig') as f:
-                # Read all lines in the file
-                lines = f.readlines()
-                # Print the contents of the .tux file
-                print(lines)
+        if ext == '.tux': # Check if the file extension is '.tux'
+            with open(filepath, 'r', encoding='utf-8-sig') as f: # Open the .tux file in read mode with the specified encoding
+                lines = f.readlines() # Read all lines in the file
+                print(lines) # Print the contents of the .tux file
         else:
-            # Inform the user if the file is not a .tux file
-            print('The file is not a .tux file')
+            print('The file is not a .tux file') # Inform the user if the file is not a .tux file
     
-    # Check if the path is a directory
-    elif os.path.isdir(filepath):
+    elif os.path.isdir(filepath): # Check if the path is a directory
         print('The path is a directory. Here are its .tux files:')
-        # Loop through each file in the directory
-        for filename in os.listdir(filepath):
-            # Check if the file has a .tux extension
-            if filename.endswith('.tux'):
-                # Print the name of the .tux file
-                print(filename)
+        for filename in os.listdir(filepath): # Loop through each file in the directory
+            if filename.endswith('.tux'): # Check if the file has a .tux extension
+                print(filename) # Print the name of the .tux file
     
-    # Handle the case where the path is neither a file nor a directory
-    else:
+    else: # Handle the case where the path is neither a file nor a directory
         print('The specified path does not exist')
 else:
-    # Inform the user if the path does not exist
-    print('Please provide a path.')
+    print('Please provide a path.') # Inform the user if the path does not exist
 
